e4872c456ba0342fb6a28b680b0412dc78a46304
João Elias Maccari <joaomaccari@hotmail.com>
2022-05-06 22:21:20 -0300

    att2 proketo1

---
+++ Projeto001/js/script.js
@@ -0,0 +1,11 @@
+var listitens1 = document.getElementsByClassName("list-item")[0]
+var listitens2 = document.getElementsByClassName("list-item")[1]
+var listitens3 = document.getElementsByClassName("list-item")[2]
+var listitens4 = document.getElementsByClassName("list-item")[3]
+var listitens5 = document.getElementsByClassName("list-item")[4]
+
+
+
+
+
+console.log()
